97e48353b3606af716a2e2cde0f994b8f1ec4c4b
Inaya420 <Hafizulislam3829@gmail.com>
2026-08-31 23:23:24 +0600

    Update Imgur link in Admin.js

--- Script/commands/Admin.js
+++ Script/commands/Admin.js
@@ -100,7 +100,7 @@ module.exports.run = async function({ api, event }) {
  attachment: fs.createReadStream(__dirname + "/cache/owner.jpg")
  }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/owner.jpg"));
 
- return request(" https://i.imgur.com/gokzyKd.jpeg ") //এখানে আপনার ছবির Imgur link বসাবেন✅
+ return request("https://imgur.com/6MJJVUl") //এখানে আপনার ছবির Imgur link বসাবেন✅
  .pipe(fs.createWriteStream(__dirname + '/cache/owner.jpg'))
  .on('close', () => callback());
 };") //এখানে আপনার ছবির Imgur link বসাবেন✅
